cc11af4d980f224d559d016b1b499cd964796704
junhaohogc123 <tjunhao17@gmail.com>
2020-11-10 02:08:59 +0800

    update

---
+++ 27_Junhao_Lab9/Assets/Scripts/Character.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Net.Mime;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Character : MonoBehaviour
+{
+    public GameObject PosText;
+    public float speed;
+    public float turningspeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            //transform.position += new Vector3(0, 0, Time.deltaTime * speed);
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            //transform.position += new Vector3(0, 0, Time.deltaTime * -speed);
+            transform.position += transform.forward * -speed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, Time.deltaTime * turningspeed, 0));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, Time.deltaTime * -turningspeed, 0));
+        }
+        PosText.GetComponent<Text>().text = ("Position: " + (GameObject.FindGameObjectWithTag("Player").transform.position));
+    }
+}
